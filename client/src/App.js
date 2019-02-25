b1ea5608ceb12ab691316f7c46962eaa29e015c0
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-25 16:51:38 -0500

    Two routes are created

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom'; 
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state = { 
       savedList: []
     };
   }
@@ -22,7 +23,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path = '/' component ={MovieList}> </Route>
+        <Route path = '/movies/:id' component = {Movie}></Route>
       </div>
     );
   }
